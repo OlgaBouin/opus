b6f6f1c9466b2e12a5572effa049d536cd54f08e
OlgaBouin <80254665+OlgaBouin@users.noreply.github.com>
2021-03-10 16:06:07 +0100

    Add phone nb checker

--- script.sh
+++ script.sh
@@ -7,3 +7,9 @@ message='Bonjour tout le monde'
 echo $message
 
 
+echo "enter a phone number"
+read phone
+
+regex="^((\+|00)[0-9]{3}|[0-9]{2})([\. -]?[0-9]{2}){4}$"
+
+[[ $phone =~ ^regex$ ]] && echo "matched" || echo "did not match"
